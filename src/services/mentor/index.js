11fe75dfd561ab7b0eba205c0ac495f521328638
sirleyalmeida <sirleyas90@gmail.com>
2022-09-01 22:55:46 -0300

    fix: service

--- src/services/mentor/index.js
+++ src/services/mentor/index.js
@@ -1,51 +1,22 @@
 import axios from 'axios';
 
-export const createMentor = () => {
+export const createMentor = (name, email, password, age, cpf, profession, practiceTime, practiceArea, education, userType) => {
     axios.post('http://localhost:3333/create-mentor', {
-        "name": "Mentor teste",
-        "email": "mentor@teste.com",
-        "password": "string",
-        "age": 40,
-        "document": "11717538029",
-        "profession": "matematico",
-        "practiceArea": "educação",
-        "practiceTime": "12",
-        "education": "superior completo",
-        "userType": "mentor"
+        name: name,
+        email: email,
+        password: password,
+        age: age,
+        document: cpf,
+        profession: profession,
+        practiceArea: practiceArea,
+        practiceTime: practiceTime,
+        education: education,
+        userType: userType
     })
     .then(function (response) {
     console.log(response);
-    console.log('bateu caralho');
     })
     .catch(function (error) {
     console.log(error);
     });
 };
-
-createMentor();
-
-// {
-//     "name": "string",
-// "email": "string",
-// "password": "string",
-// "age": 20,
-// "document": "string",
-// "profession": "string",
-// "practiceArea": "string",
-// "practiceTime": "string",
-// "education": "string",
-// "userType": "string"
-// }
-
-// {
-// 	"uuid": "d2c416d0-2059-4321-b4be-afb12e83b186",
-// 	"email": "string",
-// 	"name": "string",
-// 	"age": 20,
-// 	"document": "string",
-// 	"profession": "string",
-// 	"practiceArea": "string",
-// 	"practiceTime": "string",
-// 	"education": "string",
-// 	"userType": "string"
-// }
