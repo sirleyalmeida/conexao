4da945c5ef07dd97e4ab7d8b1efafe83a0a28c5a
“Tatiane <imeio.tmc@gmail.com>
2022-09-26 23:15:28 +0200

    mentored update created and profile done

--- src/services/mentored/index.js
+++ src/services/mentored/index.js
@@ -22,3 +22,23 @@ export const fetchMentored = (uuid) =>
 	.catch(function (error) {
 		console.log(error);
 	});
+
+export const updateMentored = ({ name, email, password, age, document, profession, interestArea, education, mentorshipGoal, userType}, uuid) => 
+	axios.put(`http://localhost:3333/mentored/${uuid}`, {
+		name: name, 
+		email: email, 
+		password: password,
+		age: age, 
+		document: document, 
+		profession: profession, 
+		interestArea: interestArea, 
+		education: education,
+		mentorshipGoal: mentorshipGoal, 
+		userType: userType
+	})
+	.then(function (response) {
+		return response
+	})
+	.catch(function (error) {
+		console.log(error);
+	});
